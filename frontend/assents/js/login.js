8e060864ef1953a7ca5da523072b8f2752dd2793
Bautista <giglibauti889@gmail.com>
2026-09-23 20:49:49 -0300

    feat: Capacidad 1 completo

--- frontend/assents/js/login.js
+++ frontend/assents/js/login.js
@@ -1,23 +1,57 @@
 //EL LOGIN VALIDA
 const form = document.querySelector('#form-login');
-const cajaError = document.querySelector('#error-login');
 
 form.addEventListener('submit', function (evento){
     evento.preventDefault();
     console.log('se envió');
+
     const email = document.querySelector('#input-email').value;
     const password = document.querySelector('#input-password').value;
 
-    
+    const msjEmail = document.querySelector('#error-email');
+    const msjPassword = document.querySelector('#error-password');
+
+
+    msjEmail.textContent = '';
+    msjPassword.textContent = '';
+
     console.log(email, password);
-    if (email == '' || password === ''){
-    cajaError.textContent = 'Completá email y contraseña.';
-    cajaError.classList.add('visible');
-    return;
+
+    let hayError = false;
+
+    if (email === ''){
+        msjEmail.textContent = 'El email es obligatorio.';
+        hayError = true;
+    } else {
+        
+    let tieneArroba = false;
+    let tienePunto = false;
+
+    for (let i = 0; i < email.length; i = i + 1){
+        if (email[i] === '@'){
+            tieneArroba = true;
+        }
+        if (email[i] === '.'){
+            tienePunto = true;
+        }
     }
 
-    cajaError.textContent = '';
-    cajaError.classList.remove('visible');
+    if(tieneArroba === false || tienePunto === false){
+        msjEmail.textContent = 'Email invalido debe contener un @ y un punto.';
+        hayError = true;
+    }
 
-    window.location.href = "index.html";
+    }
+
+    if (password === ''){
+        msjPassword.textContent = 'La contraseña es obligatoria.';
+        hayError = true;
+    } 
+
+    if (hayError === true){
+        return;
+    }
+
+    window.open('index.html', '_self');
+
 });
